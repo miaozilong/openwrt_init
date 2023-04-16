d4f6f1be069ddbf0326efdb6c1cebc5500c4b461
miaozilong <miao.zilong@outlook.com>
2023-04-16 23:27:39 +0800

    重新整理代码

---
+++ check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+limit_date_file=/mofahezi/limit_date.txt
+mkdir /tmp/log -p
+if [ -f "$limit_date_file" ]; then
+  echo "限定日期已存在" >/tmp/log/check.log
+else
+  cur_sec=$(date '+%s')
+  limit_sec=$((cur_sec + 375 * 24 * 60 * 60))
+  limit_date=$(date -d @${limit_sec})
+  str_limit=$(date -d @$limit_sec "+%Y-%m-%d %H:%M:%S")
+  echo "${str_limit}" >${limit_date_file}
+  echo "限定日期不存在，写入日期:${str_limit}" >/tmp/log/check.log
+fi
+#一直检测是否过期
+while true; do
+  # 30*60 每30分钟检测一次
+  # todo 00
+  sleep 18
+  str_limit=$(cat ${limit_date_file})
+  str_now=$(date "+%Y-%m-%d %H:%M:%S")
+  date_limit=$(date -d "$str_limit" +%s)
+  date_now=$(date -d "$str_now" +%s)
+  if [ $date_limit -gt $date_now ]; then
+    echo "现在时间:${str_now},过期时间:${str_limit}没有过期" >>/tmp/log/check.log
+  else
+    echo "现在时间:${str_now},过期时间:${str_limit}过期了直接关机" >>/tmp/log/check.log
+    # todo
+    #    halt
+  fi
+done
---
+++ init.sh
@@ -0,0 +1,7 @@
+#chmod +x /mofahezi/init.sh
+#nohup /mofahezi/init.sh >/dev/null 2>&1 &
+#exit 0
+
+chmod +x /mofahezi/check.sh
+nohup sh /mofahezi/check.sh >/dev/null 2>&1 &
+exit 0
